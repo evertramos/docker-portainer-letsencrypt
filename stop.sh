ed1684c86abc15d4ba0aa9e3ec50d96ab4d53fb2
codespearhead <72931357+Paguiar735@users.noreply.github.com>
2023-10-22 22:23:55 -0400

    fix: run docker compose instead of docker-compose
    The version without a dash doesn't break docker compose v2

--- stop.sh
+++ stop.sh
@@ -5,6 +5,6 @@
 #
 # Uses the admin password specified in the .env file
 DOCKER_COMPOSE_ARGS="-f docker-compose.yml down"
-docker-compose $DOCKER_COMPOSE_ARGS
+docker compose $DOCKER_COMPOSE_ARGS
 
 exit 0
